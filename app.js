987b4221f54dde7f2709d36fd2424b3eaa99333d
Revanth Gowda Vimala Kempegowda <vkrevanthgowda@gmail.com>
2023-03-14 18:58:40 -0500

    Final

---
+++ app.js
@@ -0,0 +1,17 @@
+// select modal-btn,modal-overlay,close-btn
+// listen for click events on modal-btn and close-btn
+// when user clicks modal-btn add .open-modal to modal-overlay
+// when user clicks close-btn remove .open-modal from modal-overlay
+
+let modalBtn = document.querySelector('.modal-btn');
+let modalOverlay = document.querySelector('.modal-overlay');
+let closeBtn = document.querySelector('.close-btn');
+
+modalBtn.addEventListener('click',function(){
+    console.log(modalOverlay.classList);
+    modalOverlay.classList.add('open-modal');
+});
+
+closeBtn.addEventListener('click',function(){
+    modalOverlay.classList.remove("open-modal");
+});
